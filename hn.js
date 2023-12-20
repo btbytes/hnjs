3c0065343ff08ea4c2d11d1d5f2d3447698f3456
Pradeep Gowda <pradeep@btbytes.com>
2023-12-20 19:12:27 +0530

    ChatGPT Prompt: rewrite this code to use ES6 features
    ChatGPT comment (along with code):

--- hn.js
+++ hn.js
@@ -1,149 +1,125 @@
-function $(id) {
-  return document.getElementById(id);
-}
-function byClass(el, cl) {
-  return el ? el.getElementsByClassName(cl) : [];
-}
-function byTag(el, tg) {
-  return el ? el.getElementsByTagName(tg) : [];
-}
-function allof(cl) {
-  return byClass(document, cl);
-}
-function classes(el) {
-  return (el && el.className && el.className.split(" ")) || [];
-}
-function hasClass(el, cl) {
-  return afind(cl, classes(el));
-}
-function addClass(el, cl) {
+const $ = (id) => document.getElementById(id);
+
+const byClass = (el, cl) => (el ? el.getElementsByClassName(cl) : []);
+
+const byTag = (el, tg) => (el ? el.getElementsByTagName(tg) : []);
+
+const allof = (cl) => byClass(document, cl);
+
+const classes = (el) => (el && el.className && el.className.split(" ")) || [];
+
+const hasClass = (el, cl) => afind(cl, classes(el));
+
+const addClass = (el, cl) => {
   if (el) {
-    var a = classes(el);
+    const a = classes(el);
     if (!afind(cl, a)) {
       a.unshift(cl);
       el.className = a.join(" ");
     }
   }
-}
-function remClass(el, cl) {
+};
+
+const remClass = (el, cl) => {
   if (el) {
-    var a = classes(el);
+    const a = classes(el);
     arem(a, cl);
     el.className = a.join(" ");
   }
-}
-function uptil(el, f) {
-  if (el) return f(el) ? el : uptil(el.parentNode, f);
-}
-function upclass(el, cl) {
-  return uptil(el, function (x) {
-    return hasClass(x, cl);
-  });
-}
-function html(el) {
-  return el ? el.innerHTML : null;
-}
-function attr(el, name) {
-  return el.getAttribute(name);
-}
-function tonum(x) {
-  var n = parseFloat(x);
+};
+
+const uptil = (el, f) => (el ? (f(el) ? el : uptil(el.parentNode, f)) : null);
+
+const upclass = (el, cl) => uptil(el, (x) => hasClass(x, cl));
+
+const html = (el) => (el ? el.innerHTML : null);
+
+const attr = (el, name) => el.getAttribute(name);
+
+const tonum = (x) => {
+  const n = parseFloat(x);
   return isNaN(n) ? null : n;
-}
-function remEl(el) {
-  el.parentNode.removeChild(el);
-}
-function posf(f, a) {
-  for (var i = 0; i < a.length; i++) {
+};
+
+const remEl = (el) => el.parentNode.removeChild(el);
+
+const posf = (f, a) => {
+  for (let i = 0; i < a.length; i++) {
     if (f(a[i])) return i;
   }
   return -1;
-}
-function apos(x, a) {
-  return typeof x == "function"
-    ? posf(x, a)
-    : Array.prototype.indexOf.call(a, x);
-}
-function afind(x, a) {
-  var i = apos(x, a);
+};
+
+const apos = (x, a) =>
+  typeof x == "function" ? posf(x, a) : Array.prototype.indexOf.call(a, x);
+
+const afind = (x, a) => {
+  const i = apos(x, a);
   return i >= 0 ? a[i] : null;
-}
-function acut(a, m, n) {
-  return Array.prototype.slice.call(a, m, n);
-}
-function aeach(fn, a) {
-  return Array.prototype.forEach.call(a, fn);
-}
-function arem(a, x) {
-  var i = apos(x, a);
+};
+
+const acut = (a, m, n) => Array.prototype.slice.call(a, m, n);
+
+const aeach = (fn, a) => Array.prototype.forEach.call(a, fn);
+
+const arem = (a, x) => {
+  const i = apos(x, a);
   if (i >= 0) {
     a.splice(i, 1);
   }
   return a;
-}
-function alast(a) {
-  return a[a.length - 1];
-}
-function vis(el, on) {
+};
+
+const alast = (a) => a[a.length - 1];
+
+const vis = (el, on) => {
   if (el) {
-    (on ? remClass : addClass)(el, "nosee");
+    on ? remClass(el, "nosee") : addClass(el, "nosee");
   }
-}
-function setshow(el, on) {
-  (on ? remClass : addClass)(el, "noshow");
-}
-function noshow(el) {
-  setshow(el, false);
-}
+};
 
-function ind(tr) {
-  var el = byClass(tr, "ind")[0];
+const setshow = (el, on) => {
+  on ? remClass(el, "noshow") : addClass(el, "noshow");
+};
+
+const noshow = (el) => setshow(el, false);
+
+const ind = (tr) => {
+  const el = byClass(tr, "ind")[0];
   return el ? tonum(attr(el, "indent")) : null;
-}
+};
 
-function vurl(id, how, auth, _goto) {
-  return (
-    "vote?id=" +
-    id +
-    "&how=" +
-    how +
-    "&auth=" +
-    auth +
-    "&goto=" +
-    encodeURIComponent(_goto) +
-    "&js=t"
-  );
-}
+const vurl = (id, how, auth, _goto) =>
+  `vote?id=${id}&how=${how}&auth=${auth}&goto=${encodeURIComponent(
+    _goto,
+  )}&js=t`;
 
-function vote(id, how, auth, _goto) {
-  vis($("up_" + id), how == "un");
-  vis($("down_" + id), how == "un");
-  var unv = "";
+const vote = (id, how, auth, _goto) => {
+  vis($(`up_${id}`), how == "un");
+  vis($(`down_${id}`), how == "un");
+  let unv = "";
   if (how != "un") {
-    unv =
-      " | <a id='un_" +
-      id +
-      "' class='clicky' " +
-      "href='" +
-      vurl(id, "un", auth, _goto) +
-      "'>" +
-      (how == "up" ? "unvote" : "undown") +
-      "</a>";
+    unv = ` | <a id='un_${id}' class='clicky' href='${vurl(
+      id,
+      "un",
+      auth,
+      _goto,
+    )}'>${how == "up" ? "unvote" : "undown"}</a>`;
   }
-  $("unv_" + id).innerHTML = unv;
+  $(`unv_${id}`).innerHTML = unv;
   new Image().src = vurl(id, how, auth, _goto);
-}
+};
 
-function nextcomm(el) {
+const nextcomm = (el) => {
   while ((el = el.nextElementSibling)) {
     if (hasClass(el, "comtr")) return el;
   }
-}
+};
 
-function kidvis(tr, show) {
-  var n0 = ind(tr),
-    n = ind(nextcomm(tr)),
-    coll = false;
+const kidvis = (tr, show) => {
+  const n0 = ind(tr);
+  const n = ind(nextcomm(tr));
+  let coll = false;
   if (n > n0) {
     while ((tr = nextcomm(tr))) {
       if (ind(tr) <= n0) {
@@ -158,84 +134,78 @@ function kidvis(tr, show) {
       }
     }
   }
-}
+};
 
-function toggleCollapse(id) {
-  var tr = $(id),
-    coll = !hasClass(tr, "coll");
+const toggleCollapse = (id) => {
+  const tr = $(id);
+  const coll = !hasClass(tr, "coll");
   collstate(tr, coll);
   kidvis(tr, !coll);
   if ($("logout")) {
-    new Image().src = "collapse?id=" + id + (coll ? "" : "&un=true");
+    new Image().src = `collapse?id=${id}${coll ? "" : "&un=true"}`;
   }
-}
+};
 
-function collstate(tr, coll) {
-  (coll ? addClass : remClass)(tr, "coll");
+const collstate = (tr, coll) => {
+  coll ? addClass(tr, "coll") : remClass(tr, "coll");
   vis(byClass(tr, "votelinks")[0], !coll);
   setshow(byClass(tr, "comment")[0], !coll);
-  var el = byClass(tr, "togg")[0];
-  el.innerHTML = coll ? "[" + attr(el, "n") + " more]" : "[–]";
-}
+  const el = byClass(tr, "togg")[0];
+  el.innerHTML = coll ? `[${attr(el, "n")} more]` : "[–]";
+};
 
-function onop() {
-  return attr(byTag(document, "html")[0], "op");
-}
+const onop = () => attr(byTag(document, "html")[0], "op");
 
-function ranknum(el) {
-  var s = html(el) || "";
-  var a = s.match(/[0-9]+/);
+const ranknum = (el) => {
+  const s = html(el) || "";
+  const a = s.match(/[0-9]+/);
   if (a) {
     return tonum(a[0]);
   }
-}
+};
 
-var n1 = ranknum(allof("rank")[0]) || 1;
+const n1 = ranknum(allof("rank")[0]) || 1;
 
-function newstory(pair) {
+const newstory = (pair) => {
   if (pair) {
-    var sp = alast(allof("spacer"));
-    sp.insertAdjacentHTML("afterend", pair[0] + sp.outerHTML);
+    const sp = alast(allof("spacer"));
+    sp.insertAdjacentHTML("afterend", `${pair[0]}${sp.outerHTML}`);
     fixranks();
     if (onop() == "newest") {
-      var n = ranknum(alast(allof("rank")));
-      allof("morelink")[0].href = "newest?next=" + pair[1] + "&n=" + (n + 1);
+      const n = ranknum(alast(allof("rank")));
+      allof("morelink")[0].href = `newest?next=${pair[1]}&n=${n + 1}`;
     }
   }
-}
+};
 
-function fixranks() {
-  var rks = allof("rank");
-  aeach(function (rk) {
-    rk.innerHTML = apos(rk, rks) + n1 + ".";
+const fixranks = () => {
+  const rks = allof("rank");
+  aeach((rk) => {
+    rk.innerHTML = `${apos(rk, rks) + n1}.`;
   }, rks);
-}
+};
 
-function moreurl() {
-  return allof("morelink")[0].href;
-}
-function morenext() {
-  return tonum(moreurl().split("next=")[1]);
-}
+const moreurl = () => allof("morelink")[0].href;
+const morenext = () => tonum(moreurl().split("next=")[1]);
 
-function hidestory(el, id) {
-  for (var i = 0; i < 3; i++) {
+const hidestory = (el, id) => {
+  for (let i = 0; i < 3; i++) {
     remEl($(id).nextSibling);
   }
   remEl($(id));
   fixranks();
-  var next = onop() == "newest" && morenext() ? "&next=" + morenext() : "";
-  var url = el.href.replace("hide", "snip-story").replace("goto", "onop");
-  fetch(url + next)
+  const next = onop() == "newest" && morenext() ? `&next=${morenext()}` : "";
+  const url = el.href.replace("hide", "snip-story").replace("goto", "onop");
+  fetch(`${url}${next}`)
     .then((r) => r.json())
     .then(newstory);
-}
+};
 
-function onclick(ev) {
-  var el = upclass(ev.target, "clicky");
+const onclick = (ev) => {
+  const el = upclass(ev.target, "clicky");
   if (el) {
-    var u = new URL(el.href, location);
-    var p = u.searchParams;
+    const u = new URL(el.href, location);
+    const p = u.searchParams;
     if (u.pathname == "/vote") {
       vote(p.get("id"), p.get("how"), p.get("auth"), p.get("goto"));
     } else if (u.pathname == "/hide") {
@@ -250,6 +220,6 @@ function onclick(ev) {
     ev.preventDefault();
     return false;
   }
-}
+};
 
 document.addEventListener("click", onclick);
